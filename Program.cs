d2eef1fc46559716f85e409044ddc515a1f06609
juhriber <j.de.s.r.25@gmail.com>
2021-03-19 16:03:10 -0300

    Finalização do arquivo GrausFC

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius;
+            double Fahrenheit = 32;
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Digite seu Valor em Celsius: ");
+            Celsius = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Fahrenheit = Celsius * 1.8 + 32;
+            Console.WriteLine("Celsius para Fahrenheit = {0}", Fahrenheit);
+            Console.Write("\n");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.Write("\n");
+            Console.ReadKey();
+        }
+    }
+}
